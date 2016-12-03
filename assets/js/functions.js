b268270be7bdac40c4f1a83db384a3354b4d7a91
Nino Pintaric <ninopintaric01@gmail.com>
2016-12-03 16:45:16 +0100

    7.3: Mentoring responsive

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,12 +6,23 @@ function mentoringBubbleClick() {
   $('.face').on('click',function() {
     var $this = $(this),
         faceTop = $this.position().top,
-        vertMath = -1 * (faceTop - 230);
+        vertMath = -1 * (faceTop - 230),
+        faceLeft = $this.position().left,
+        horizMath = 0 - faceLeft;
 
-    $this.parent().css('top', + vertMath + 'px');
-
-    $this.addClass('has-bubble-open')
-      .siblings().removeClass('has-bubble-open');
+    if($(window).width() > 640) {
+      $this.parent().css('top', + vertMath + 'px');
+    } else {
+      if($this.hasClass('back-btn')) {
+        mentoringNarrowStart();
+      } else {
+        $this.parent().css('left', + horizMath + 'px');
+      }
+    }
+    if(!$this.hasClass('back-btn')) {
+      $this.addClass('has-bubble-open')
+        .siblings().removeClass('has-bubble-open');
+    }
   });
 }
 
@@ -28,9 +39,43 @@ function youtubeVidScroll() {
 function startMentoring() {
   var wScroll = $(window).scrollTop();
   if($('section.mentoring').offset().top - 500 < wScroll) {
-    $('.faces').addClass('launched');
-    setTimeout(function() {
-      $('.face:nth-child(3)').addClass('has-bubble-open');
-    }, 400);
+    if($(window).width() > 640) {
+      $('.faces').addClass('launched');
+
+      if(!$('.face').hasClass('has-bubble-open')) {
+        setTimeout(function() {
+          $('.face:nth-child(3)').addClass('has-bubble-open');
+        }, 400);
+      }
+    } else {
+      mentoringNarrowStart();
+    }
   }
+}
+
+
+$(window).resize(function() {
+  if($(window).width() > 640) {
+    mentoringWideStart();
+  } else {
+    mentoringNarrowStart();
+  }
+});
+
+function mentoringNarrowStart() {
+  $('.faces').css({
+    'top': '230px',
+    'left': '0px'
+  });
+  $('.face').first().addClass('has-bubble-open')
+  .siblings().removeClass('has-bubble-open');
+}
+
+function mentoringWideStart() {
+  $('.faces').css({
+    'top': '0px',
+    'left': '0px'
+  });
+  $('.face:nth-child(3)').addClass('has-bubble-open')
+  .siblings().removeClass('has-bubble-open');
 }
