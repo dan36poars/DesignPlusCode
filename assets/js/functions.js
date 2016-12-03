2e2f74824b2449b33daa7a5837015e3542b581d8
Nino Pintaric <ninopintaric01@gmail.com>
2016-12-03 11:13:45 +0100

    6.2: YouTube section

---
+++ assets/js/functions.js
@@ -0,0 +1,8 @@
+$(window).scroll(function() {
+  youtubeVidScroll();
+});
+
+function youtubeVidScroll() {
+  var wScroll = $(window).scrollTop();
+  $('.video-strip').css('background-position','center -'+ wScroll +'px');
+}
